4a4eaecb7d8c41aa40ec0d27e2a3170109f9395a
Olalaakuma <olalaakuma@gmail.com>
2023-06-22 20:51:32 +0100

    added a signup page with component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Homepage from './pages/homepage/Homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header-component/header.component.jsx';
 import SignInAndSignUp from './pages/signin-and-signup/signin-and-signup';
-import { auth } from './firebase-utilities/firebase.util';
+import { auth, CreateUserProfileDocument } from './firebase-utilities/firebase.util';
 import '/Users/mac/fashion-store/src/App.css';
 
 
@@ -23,7 +23,24 @@ this.state = {
   unsubscribeFromAuth = null;
 
 componentDidMount(){
-this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>{this.setState({currentUser: user})});
+this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth =>{
+  if(userAuth){
+  const userRef = await CreateUserProfileDocument(userAuth);
+
+  userRef.onSnapshot(snapShot =>{
+this.setState({currentUser:{
+id: snapShot.id,
+...snapShot.data()
+
+}})
+
+  });
+}else{
+
+  this.setState({currentUser: userAuth});
+}
+
+});
 
 };
 
--- src/firebase-utilities/firebase.util.js
+++ src/firebase-utilities/firebase.util.js
@@ -12,6 +12,37 @@ const Config = {
     measurementId: "G-9HFJWHEC5P"
   };
 
+ export const CreateUserProfileDocument =async (userAuth, additionalData)=>{
+ if(!userAuth) return;
+
+ const userRef = firestore.doc(`users/ ${userAuth.uid}`);
+ const snapShot = await userRef.get();
+
+if(!snapShot.exists){
+
+const{displayName, email} = userAuth;
+const createdAt = new Date();
+
+try{
+  await userRef.set({
+   displayName,
+   email,
+   createdAt,
+   ...additionalData
+
+  })
+
+} catch(error){
+  console.log('error creating user', error.message);
+
+
+}
+}
+return userRef;
+
+  }
+  
+
   firebase.initializeApp(Config);
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
